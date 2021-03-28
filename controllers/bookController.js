a84aa0d6a3eba620c7f4512d04e9a2c182b971ea
mischadab <mischadabrowski@gmail.com>
2021-03-28 12:47:08 -0400

    create book

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -14,5 +14,16 @@ module.exports = {
         db.Book.findById(req.params.id)
         .then(bookDb => res.json(bookDb))
         .catch(err => res.json(err))
+    },
+    // create a new entry
+    create: (req, res) => {
+        db.Book.create(req.body)
+         .then(bookDb => res.json(bookDb))
+         .catch(err => res.json(err))
+        }
+    }
+    // update book
+    update: (req, res) => {
+        db.Book.findOneAndUpdate({ id: req.params.id }, req.body)
     }
 }
