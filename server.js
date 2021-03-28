ed363e8b9ee758b14667e1b100cd559db6f4d0df
mischadab <mischadabrowski@gmail.com>
2021-03-28 12:23:33 -0400

    requirements to server

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// configure body
