b0b86ea028b75eef11de9b8b197a1d17f6aecfa8
mischadab <mischadabrowski@gmail.com>
2021-03-28 12:41:59 -0400

    find book by id

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -3,8 +3,15 @@ const db = require('../models');
 // methods for the book controller
 
 module.exports = {
+    // get all books in the database
     findAll: (req, res) => {
         db.Book.find(req.query)
+        .then(bookDb => res.json(bookDb))
+        .catch(err => res.json(err))
+    },
+    // find one book using id in database
+    findById: (req, res) => {
+        db.Book.findById(req.params.id)
         .then(bookDb => res.json(bookDb))
         .catch(err => res.json(err))
     }
