0fdc8eb08326dbbe3c6dcb4ccfc0f2a236cd24f4
mischadab <mischadabrowski@gmail.com>
2021-03-28 12:25:02 -0400

    server

--- server.js
+++ server.js
@@ -6,4 +6,27 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// configure body
+// Configure body parsing for AJAX requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI,
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }
+);
+// Start the API server
+app.listen(PORT, () =>
+  console.log(` API Server listening on PORT ${PORT}!`)
+);
