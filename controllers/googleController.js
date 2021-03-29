0bc278c24fe689fa6e267a1eecab8c7702450391
mischadab <mischadabrowski@gmail.com>
2021-03-29 16:19:41 -0400

    books api filter

--- controllers/googleController.js
+++ controllers/googleController.js
@@ -8,11 +8,25 @@ module.exports = {
         axios
             .get(`https://www.googleapis.com/books/v1/volumes?q=${params}&orderBy=newest&langRestrict=en&maxResults=9`)
             .then(results =>
-                results.data.items.filter(
-                    result => {
-                        result.volumeInfo.title
-                    })
+                results.data.items.filter((result) => {
+                    result.volumeInfo.title &&
+                        result.volumeInfo.infoLink &&
+                        result.volumeInfo.authors &&
+                        result.volumeInfo.description &&
+                        result.volumeInfo.imageLinks &&
+                        result.volumeInfo.thumbnail
+                })
 
             )
+            .then((booksApi) =>
+                db.Book.find().then((booksDb) =>
+                    booksApi.filter((bookApi) =>
+                        booksDb.every((bookDb) =>
+                            bookDb.googleId.toString() !== bookApi.id)
+                    )
+                )
+            )
+            .then(books => res.json(books))
+            .catch(err => res.status(422).json(err))
     }
 }
