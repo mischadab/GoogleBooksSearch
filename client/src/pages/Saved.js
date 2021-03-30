894b924b67e046decd57661c31efe4b64977cc40
mischadab <mischadabrowski@gmail.com>
2021-03-29 21:11:57 -0400

    render html

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -4,10 +4,22 @@ class Saved extends Component {
     state = {
         savedBooks: []
     }
-}
+
 
 componentDidMount() {
     API.savedBook()
     .then(savedBooks => this.setState({ savedBooks: savedBooks }))
     .catch(err => console.error(err))
 }
+
+render() {
+    return (
+        <div className="container">
+            <h2>Saved Books</h2>
+            <Results books={this.state.savedBooks} />
+        </div>
+    )
+}
+}
+
+export default Saved;
