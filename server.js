71afb66767966970b7a08f52f9b45884e1455038
mischadab <mischadabrowski@gmail.com>
2021-03-29 20:51:52 -0400

    mogodb

--- server.js
+++ server.js
@@ -19,8 +19,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI,
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
   {
     useCreateIndex: true,
     useNewUrlParser: true
