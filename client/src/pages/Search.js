bd5aa90bc47f2398456e22ef5004984dcbda4857
mischadab <mischadabrowski@gmail.com>
2021-03-29 21:13:12 -0400

    reqs

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,4 +1,7 @@
-import API from '../utils/API'
+import React from "react";
+import Form from "../components/Form";
+import Results from "../components/Results";
+import API from "../utils/API";
 
 class Search extends React.Component {
     state = {
